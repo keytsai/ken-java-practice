c0d4b38cf4e00de694af0f98d986f03479a8140c
keytsai <keytsai513@gmail.com>
2021-05-04 09:33:04 +0800

    add IndexController

---
+++ src/main/java/com/ckmates/java/practice2/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.ckmates.java.practice2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class IndexController {
+
+  @GetMapping
+  String index() {
+    return "redirect:/memo";
+  }
+}
